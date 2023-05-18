2a22c0eea58a3bd6b7fa801176fc03b85664db99
caseyclinch <clinchcasey@gmail.com>
2023-05-17 22:03:03 -0700

    Completed Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', function (event) {
+    event.preventDefault()
+    createTask(document.querySelector('#new-task-description').value)
+  })
 });
+
+function createTask (input) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'X'
+  p.textContent = input
+  p.append(btn)
+  document.querySelector('#list').appendChild(p)
+}
